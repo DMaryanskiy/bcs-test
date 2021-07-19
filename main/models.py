80d56b6070672ef503131c77aa244bc6e3982500
Денис Марьянский <d.maryanskiy@gmail.com>
2021-07-19 11:48:21 +0300

    Heroku update

--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from .timestamp import UnixTimestampField
 
 class Blockchain_blocks(models.Model):
     height = models.PositiveIntegerField(null=True)
     hash = models.CharField(max_length=64, null=True)
-    timestamp = models.IntegerField(null=True)
+    timestamp = UnixTimestampField()
     miner = models.CharField(max_length=80, null=True)
     transactionCount = models.PositiveIntegerField(null=True)  
