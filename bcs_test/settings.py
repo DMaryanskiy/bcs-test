a283f731eb5bbae8d4d5121dde56cf93ca4b2c4c
Денис Марьянский <d.maryanskiy@gmail.com>
2021-07-19 11:22:50 +0300

    Heroku update

--- bcs_test/settings.py
+++ bcs_test/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'bcs_db',
         'USER' : 'postgres',
         'PASSWORD' : '94286130Sup',
-        'HOST' : 'bcs-task-maryanskiy.herokuapp.com',
+        'HOST' : 'localhost.',
         'PORT' : '5432',
     }
 }
