2387f03668cbc7b118518b5fa3e783071f92f5b9
Денис Марьянский <d.maryanskiy@gmail.com>
2021-07-20 13:34:06 +0300

    Timestamp change

--- main/models.py
+++ main/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from unixtimestampfield.fields import UnixTimeStampField
 
 class Blockchain_blocks(models.Model):
     height = models.PositiveIntegerField(null=True)
     hash = models.CharField(max_length=64, null=True)
-    timestamp = UnixTimeStampField(default=0.0)
+    timestamp = models.DateTimeField()
     miner = models.CharField(max_length=80, null=True)
     transactionCount = models.PositiveIntegerField(null=True)  
